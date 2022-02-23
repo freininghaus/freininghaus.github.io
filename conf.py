7112d213a6531ece37db401ae0e7e06b8784ba68
Frank Reininghaus <frank78ac@googlemail.com>
2022-02-23 21:46:19 +0100

    Add Staticman comments

--- conf.py
+++ conf.py
@@ -1025,12 +1025,12 @@ RSS_COPYRIGHT_FORMATS = CONTENT_FOOTER_FORMATS
 # systems.  The following comment systems are supported by Nikola:
 #   disqus, facebook, intensedebate, isso, muut, commento, utterances
 # You can leave this option blank to disable comments.
-COMMENT_SYSTEM = ""
+COMMENT_SYSTEM = "staticman"
 # And you also need to add your COMMENT_SYSTEM_ID which
 # depends on what comment system you use. The default is
 # "nikolademo" which is a test account for Disqus. More information
 # is in the manual.
-COMMENT_SYSTEM_ID = ""
+COMMENT_SYSTEM_ID = "https://staticman-freininghaus.herokuapp.com/v3/entry/github/freininghaus/freininghaus.github.io/main/comments"
 
 # Create index.html for page folders?
 # WARNING: if a page would conflict with the index file (usually
